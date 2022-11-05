9b62f9dc6805711d143c66d1387a2f2b02e42b0c
Солотина Камилла Андреевна <k.solotina@iac.spb.ru>
2022-11-05 16:37:37 +0300

    refactoring. unused methods removed

--- src/java/com/game/pacman/utils/SoundPlayer.java
+++ src/java/com/game/pacman/utils/SoundPlayer.java
@@ -13,26 +13,6 @@ import javax.sound.sampled.Clip;
 public class SoundPlayer {
 
     /**
-     * synchronized sound, creating a new thread for parallel run
-     * @param name which sound to play
-     */
-    public static synchronized void playAsync(final String name) {
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Clip clip = AudioSystem.getClip();
-                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                            Main.class.getResourceAsStream("/sounds/" + name));
-                    clip.open(inputStream);
-                    clip.start();
-                } catch (Exception e) {
-                    System.err.println(e.getMessage());
-                }
-            }
-        }).start();
-    }
-
-    /**
      * plays the indicated sound
      * @param name which sound to play
      */
@@ -47,21 +27,5 @@ public class SoundPlayer {
             System.err.println(e.getMessage());
         }
     }
-
-    /**
-     * plays the siren sound
-     */
-    public static void startSiren(){
-        try {
-            Clip clip = AudioSystem.getClip();
-            AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                    Main.class.getResourceAsStream("/sounds/siren.wav"));
-            clip.open(inputStream);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
 
 }
